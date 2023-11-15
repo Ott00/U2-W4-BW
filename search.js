bceb43b443aa48ad83dd2eb00a01cdde5ae28e39
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-11-15 14:56:09 +0100

    search fixed

--- search.js
+++ search.js
@@ -11,8 +11,8 @@ const searchArtist = async function () {
       method: "GET",
       headers: {
         "X-RapidAPI-Key": "6969464db2msh57ee0909918148fp1b3cafjsn9608ba4cbef4",
-        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-      },
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+      }
     });
 
     if (!response.ok) {
@@ -22,41 +22,41 @@ const searchArtist = async function () {
     const responseObj = await response.json();
     console.log(responseObj);
 
-    // const allNames = responseObj.data.map((element) => {
-    //   return element.artist.name;
-    // });
-    // console.log(allNames);
-
-    // //cancellati i nomi doppioni
-    // const allNamesFiltered = [...new Set(allNames)];
-    // console.log(allNamesFiltered);
-
-    console.log(responseObj.data.length);
-
+    //Troviamo l'artista
     for (let i = 0; i < responseObj.data.length; i++) {
       if (
         responseObj.data[i].artist.name
           .toLowerCase()
           .normalize("NFD")
-          .replace(/[\u0300-\u036f]/g, "") ===
-        searchInput.value
-          .toLowerCase()
-          .normalize("NFD")
           .replace(/[\u0300-\u036f]/g, "")
+          .includes(
+            searchInput.value
+              .toLowerCase()
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+          )
       ) {
         const artist = responseObj.data[i].artist;
-        console.log(artist);
+        // console.log(artist);
 
         searchContainer.innerHTML = "";
 
+        const titleArtist = document.createElement("h2");
+        titleArtist.classList = "col-6 mb-3";
+        titleArtist.innerText = "Risultato più rilevante";
+
+        const titleSong = document.createElement("h2");
+        titleSong.classList = "col-6 mb-3";
+        titleSong.innerText = "Brani";
+
         const col = document.createElement("div");
-        col.classList = "col-5";
+        col.classList = "col-5 col-xxl-6";
 
         const artistCard = document.createElement("div");
-        artistCard.classList = "card";
+        artistCard.classList = "card border-0 bg-spotify p-3";
 
         const cardImg = document.createElement("img");
-        cardImg.classList = "card-img-top";
+        cardImg.classList = "card-img-top w-25 rounded-circle shadow-lg";
         cardImg.src = artist.picture_big;
 
         const cardBody = document.createElement("div");
@@ -67,8 +67,9 @@ const searchArtist = async function () {
         cardTitle.innerText = artist.name;
 
         const cardText = document.createElement("p");
-        cardText.classList = "card-text";
-        cardText.innerText = artist.type;
+        cardText.classList = "card-text badge bg-dark";
+        cardText.innerText =
+          artist.type.charAt(0).toUpperCase() + artist.type.slice(1);
 
         cardBody.appendChild(cardTitle);
         cardBody.appendChild(cardText);
@@ -76,6 +77,8 @@ const searchArtist = async function () {
         artistCard.appendChild(cardBody);
         col.appendChild(artistCard);
 
+        searchContainer.appendChild(titleArtist);
+        searchContainer.appendChild(titleSong);
         searchContainer.appendChild(col);
       }
     }
@@ -83,15 +86,7 @@ const searchArtist = async function () {
     // const artist = responseObj.data[0].artist;
 
     const album = responseObj.data[1].album;
-    console.log(album);
-
-    /* <div c   lass="card" >
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div> */
+    // console.log(album);
   } catch (error) {
     console.log("errore nella ricerca artista", error);
   }
