2bd16b8a8a87a933d3dee2f7e5025c8df5568f2e
Luca Utzeri <lutzeri@libero.it>
2023-11-16 16:19:25 +0100

    function for "browse all" cards color + shadow

--- color.js
+++ color.js
@@ -1,10 +1,11 @@
 const colorThief = new ColorThief();
 const img = document.getElementById('album-image');
 console.log(img)
-
+const tiltedImg = document.getElementsByClassName("rotate-image");
+console.log(tiltedImg)
 // Make sure image is finished loading
 window.onload = () => {
-    if (img.complete) {
+    if (img) {
         const color = colorThief.getColor(img);
         console.log(color)
         const colorStr = JSON.stringify(color)
@@ -13,9 +14,25 @@ window.onload = () => {
         console.log(rgbValue)
         const changeBg = document.getElementById("central-bar-container")
         changeBg.style.backgroundColor = "rgb" + rgbValue;
-    } else {
-        image.addEventListener('load', function () {
-            colorThief.getColor(img);
-        });
+    }
+    else {
+        // img.addEventListener('load', function () {
+        //     colorThief.getColor(img);
+        // });
+
+        imgArr = Array.from(tiltedImg)
+        console.log(imgArr)
+
+        imgArr.forEach((elem) => {
+            const color = colorThief.getColor(elem);
+            console.log(elem)
+            const colorStr = JSON.stringify(color)
+            console.log(colorStr)
+            const rgbValue = colorStr.replaceAll("[", "(").replaceAll("]", ")")
+            console.log(rgbValue)
+            const changeBg = elem.closest("")
+            changeBg.style.backgroundColor = "rgb" + rgbValue;
+        })
     }
 }
+
